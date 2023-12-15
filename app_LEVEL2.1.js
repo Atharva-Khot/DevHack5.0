72026602c82363fab262cdcaea0a3f861a549bc5
Kamlesh Takhar <220010025@iitdh.ac.in>
2023-12-15 09:19:37 +0530

    LEVEL3 security completed using hash function md5

---
+++ app_LEVEL2.1.js
@@ -0,0 +1,95 @@
+//jshint esversion:6
+require('dotenv').config();
+const express=require("express")
+const bodyParser=require('body-parser');
+const ejs=require('ejs');
+const mongoose=require('mongoose');
+const encrypt=require('mongoose-encryption');
+
+const app=express();
+const PORT=3000;
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+
+
+//*************************** */
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser : true});
+const userSchema=new mongoose.Schema({
+    email:String,
+    password:String,
+});
+
+
+//when save password encrpts and when find kicks password decrypts auto
+//here we use extra layer for encryption by storing our encrpting message in .env file
+// const secret=process.env.SECRET;
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+
+const User=new mongoose.model("User",userSchema);
+
+
+
+app.get("/",(req,res)=>{
+    res.render("home");
+});
+
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+
+app.get("/register",(req,res)=>{
+    res.render("register");
+});
+
+app.get("/logout",(req,res)=>{
+    res.render("home");
+});
+
+app.get("/submit",(req,res)=>{
+    res.render("submit");
+}); 
+
+
+app.post("/register", async (req, res) => {
+    const userN = req.body.username;
+    const pwd = req.body.password;
+
+    const newUser = new User({
+        email: userN,
+        password: pwd,
+    });
+
+    try {
+        await newUser.save();
+        res.render("secrets");
+    } catch (err) {
+        console.error(err);
+        // Handle the error, for example, send an error response to the client
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+
+app.post("/login",async(req,res)=>{
+    const password=req.body.password;
+    const username=req.body.username;
+
+    try{
+        const data=await User.findOne({email:username});
+        if(data){
+            if(data.password===password){
+                res.render("secrets");
+            }else{
+                res.render("home");
+            }
+        }
+    }catch(err){
+        console.send(404);
+    }
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at port ${PORT} `);
+});
